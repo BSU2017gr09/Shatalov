821847d581450e2416aecb661910ca6cdd23d5c2
vsevshatal <36421089+vsevshatal@users.noreply.github.com>
2018-04-18 09:42:49 +0300

    Add files via upload

---
+++ bus.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Bus
+{
+private:
+    char* Surname;
+    int BusNumber;
+    int Age;
+    int RouteNumber;
+public:
+    Bus():BusNumber(0), Age(0), RouteNumber(0), Surname(nullptr) {};
+    Bus(int a = 0, int b = 0, int c = 0, char* d = nullptr):BusNumber(a), Age(b), RouteNumber(c), Surname(nullptr) {
+        if (d == nullptr)
+            d = "\0";
+        Surname = new (nothrow) char[strlen(d)+1];
+        strcpy(Surname, d);
+    };
+    Bus(const Bus &other):BusNumber(other.BusNumber), Age(other.Age), RouteNumber(other.RouteNumber), Surname(nullptr) {
+        Surname = new char[strlen(other.Surname)+1];
+        strcpy(Surname, other.Surname);
+    };
+    friend ostream& operator << (ostream &out, Bus &tmp) {
+        out << tmp.BusNumber << endl << tmp.Age << endl << tmp.RouteNumber << endl << tmp.Surname << endl;
+        return out;
+    };
+    friend istream& operator >> (istream &in, Bus &num) {
+        in >> num.BusNumber >> num.Age >> num.RouteNumber >> num.Surname;
+        return in;
+    };
+    bool operator < (const Bus &other) {
+        if (other.BusNumber < BusNumber)
+            return true;
+        else return false;
+    };
+    bool operator > (const Bus &other) {
+        if (other.BusNumber > BusNumber)
+            return true;
+        else return false;
+    };
+    ~Bus() {
+        delete []Surname;
+    };
+    
+    int getBusNumbers()
+    {
+        return BusNumber;
+    }
+    int getRouteNumbers()
+    {
+        return RouteNumber;
+    }
+    int getAge()
+    {
+        return Age;
+    }
+};
+
+void printBusNumbers(Bus *arr[], int n)
+{
+    cout << "Bus numbers: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << (*arr[i]).getRouteNumbers();
+    }
+    cout << endl;
+}
+
+void sortByRouteNumber(Bus *arr[], int n)
+{
+    int a[n];
+    for (int t = 0; t < n; t++)
+        a[t] = (*arr[t]).getRouteNumbers();
+    for (int i = n-1; i>=0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (a[j] > a[j+1])
+            {
+                swap(a[j], a[j+1]);
+            }
+        }
+    }
+    cout << "Bus routes: ";
+    for (int k = 0; k < n; k++)
+    {
+        cout << a[k] << " ";
+    }
+    cout << endl;
+}
+
+void avgAge(Bus *arr[], int n)
+{
+    int a[n];
+    int s = 0;
+    for (int t = 0; t < n; t++)
+        a[t] = (*arr[t]).getAge();
+    for (int k = 0; k < n; k++)
+    {
+        s = s + a[k];
+    }
+    s = s / n;
+    cout << "Average Age: " << s << endl;
+}
+
+void oldAndYoung(Bus *arr[], int n)
+{
+    int a[n];
+    for (int t = 0; t < n; t++)
+        a[t] = (*arr[t]).getAge();
+    for (int i = n-1; i>=0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (a[j] > a[j+1])
+            {
+                swap(a[j], a[j+1]);
+            }
+        }
+    }
+    cout << "Age of youngest: " << a[0] << endl;
+    cout << "Age of oldest: " << a[n-1] << endl;
+}
+
+int main(int argc, const char * argv[]) {
+    const int N = 3;
+    Bus *arr[N];
+    Bus a(9335, 40, 154, "Vladimir");
+    Bus b(4363, 42, 536, "Kirill");
+    Bus c(2642, 56, 243, "Dmitry");
+    arr[0] = &a;
+    arr[1] = &b;
+    arr[2] = &c;
+    printBusNumbers(arr, N);
+    sortByRouteNumber(arr, N);
+    avgAge(arr, N);
+    oldAndYoung(arr, N);
+    //cout << a;
+    //cin >> a;
+    //cout << a;
+}
---
+++ time.cpp
@@ -0,0 +1,245 @@
+#include <iostream>
+using namespace std;
+
+enum DayTime {Morning, Day, Evening, Night};
+
+class Time
+{
+private:
+    int Seconds;
+    int Minutes;
+    int Hours;
+public:
+    Time(int a = 0, int b = 0, int c = 0):Hours(a), Minutes(b), Seconds(c) {};
+    Time(const Time &other) {
+        this->Seconds = other.Seconds;
+        this->Minutes = other.Minutes;
+        this->Hours = other.Hours;
+    };
+    Time(const Time &&other) {
+        this->Seconds = other.Seconds;
+        this->Minutes = other.Minutes;
+        this->Hours = other.Hours;
+    };
+    friend istream& operator >> (istream &in, Time &num) {
+        cin >> num.Hours >> num.Minutes >> num.Seconds;
+        if (num.Hours > 24 || num.Minutes > 60 || num.Seconds > 60)
+        {
+            cout << "Wrong time" << endl;
+            exit(1);
+        }
+        return in;
+    };
+    void operator = (const Time &other) {
+        this->Seconds = other.Seconds;
+        this->Minutes = other.Minutes;
+        this->Hours = other.Hours;
+    };
+    Time& operator = (Time &&other) {
+        swap(Seconds, other.Seconds);
+        swap(Minutes, other.Minutes);
+        swap(Hours, other.Hours);
+        cout << "Move" << endl;
+        return (*this);
+    };
+    friend ostream& operator << (ostream &out, Time &tmp) {
+        out << tmp.Hours << ":" << tmp.Minutes << ":" << tmp.Seconds;
+        return out;
+        
+    };
+    Time operator + (const Time &plus) {
+        Time res;
+        res.Seconds = this->Seconds + plus.Seconds;
+        res.Minutes = this->Minutes + plus.Minutes;
+        res.Hours = this->Hours + plus.Hours;
+        if (res.Seconds >= 60)
+        {
+            res.Seconds = res.Seconds - 60;
+            res.Minutes++;
+        }
+        if (res.Minutes >= 60)
+        {
+            res.Minutes = res.Minutes - 60;
+            res.Hours++;
+        }
+        if (res.Hours >= 24)
+        {
+            cout << "Can't handle that much... Please, don't exceed 24 hours" << endl;
+        }
+        return res;
+    };
+    Time operator - (Time &minus) {
+        Time res;
+        res.Seconds = this->Seconds - minus.Seconds;
+        res.Minutes = this->Minutes - minus.Minutes;
+        res.Hours = this->Hours - minus.Hours;
+        if (res.Seconds >= 60)
+        {
+            res.Seconds = res.Seconds - 60;
+            res.Minutes++;
+        }
+        if (res.Minutes >= 60)
+        {
+            res.Minutes = res.Minutes - 60;
+            res.Hours++;
+        }
+        if (res.Hours >= 24)
+        {
+            cout << "Can't handle that much... Please, don't exceed 24 hours" << endl;
+        }
+        return res;
+    };
+    bool operator < (const Time &num) {
+        if (Hours < num.Hours)
+        {
+            return true;
+        }
+        else if (Hours > num.Hours)
+            return false;
+        else
+        {
+            if (Minutes < num.Minutes)
+            {
+                return true;
+            }
+            else if (Minutes > num.Minutes)
+                return false;
+            else
+            {
+                if (Seconds < num.Seconds)
+                {
+                    return true;
+                }
+                else if (Seconds > num.Seconds)
+                    return false;
+                else return false;
+            }
+        }
+    };
+    bool operator <= (const Time &num) {
+        if (Hours <= num.Hours)
+        {
+            return true;
+        }
+        else if (Hours > num.Hours)
+            return false;
+        else
+        {
+            if (Minutes <= num.Minutes)
+            {
+                return true;
+            }
+            else if (Minutes > num.Minutes)
+                return false;
+            else
+            {
+                if (Seconds <= num.Seconds)
+                {
+                    return true;
+                }
+                else if (Seconds > num.Seconds)
+                    return false;
+                else return false;
+            }
+        }
+    };
+    bool operator > (const Time &num) {
+        if (Hours < num.Hours)
+        {
+            return false;
+        }
+        else if (Hours > num.Hours)
+            return true;
+        else
+        {
+            if (Minutes < num.Minutes)
+            {
+                return false;
+            }
+            else if (Minutes > num.Minutes)
+                return true;
+            else
+            {
+                if (Seconds < num.Seconds)
+                {
+                    return false;
+                }
+                else if (Seconds > num.Seconds)
+                    return true;
+                else return false;
+            }
+        }
+    };
+    bool operator >= (const Time &num) {
+        if (Hours < num.Hours)
+        {
+            return false;
+        }
+        else if (Hours >= num.Hours)
+            return true;
+        else
+        {
+            if (Minutes < num.Minutes)
+            {
+                return false;
+            }
+            else if (Minutes >= num.Minutes)
+                return true;
+            else
+            {
+                if (Seconds < num.Seconds)
+                {
+                    return false;
+                }
+                else if (Seconds >= num.Seconds)
+                    return true;
+                else return false;
+            }
+        }
+    };
+    ~Time() {};
+    DayTime getDayTime()
+    {
+        DayTime Time;
+        if (Hours >= 6 && Hours < 12)
+            Time = Morning;
+        if (Hours >= 12 && Hours < 18)
+            Time = Day;
+        if (Hours >= 18 && Hours < 24)
+            Time = Evening;
+        if (Hours >= 0 && Hours < 6)
+            Time = Night;
+        return Time;
+    }
+};
+
+void printTime(Time t)
+{
+    if (t.getDayTime() == 0)
+        cout << "Morning" << endl;
+    if (t.getDayTime() == 1)
+        cout << "Day" << endl;
+    if (t.getDayTime() == 2)
+        cout << "Evening" << endl;
+    if (t.getDayTime() == 3)
+        cout << "Night" << endl;
+}
+
+int main(int argc, const char * argv[]) {
+    Time a(4,13,35);
+    Time b(4,13,35);
+    Time c(5,5,5);
+    Time d = c;
+    Time tmp;
+    tmp = a + b;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << tmp << endl;
+    printTime(a);
+    if (a <= b)
+        cout << "True" << endl;
+    else cout << "False" << endl;
+    return 0;
+}
